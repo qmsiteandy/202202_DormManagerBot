6f216fcb19d208d341ce82e7aa477c359de08bf4
qmsiteandy <ht93084@gmail.com>
2022-03-04 23:08:10 +0800

    Add: 以正規式檢查輸入內容、檢查LineUID是否已連結、儲存資料

--- routers/lineClientRouter.js
+++ routers/lineClientRouter.js
@@ -22,38 +22,98 @@ lineClientRouter.post(
       //-----訊息分類-----
       if(type == "message"){
 
-        //-----初始設定-----
+        //#region -----初始設定-----
         if(message.text == "初始設定"){
         
-          // 取得template
+          //取得template
           let flexMessage = (require('../template/join-input-template.json'));
           //傳送資料到特定id使用者
           await client.pushFlex(source.userId, 'join-input-template', flexMessage);
 
           client.pushText(source.userId, '姓名:\n學號:\n第幾宿舍:\n樓層:\n幾房:')
-          // const newStudent = new Student({
-          //   name: "ABC",
-          //   userID: "110011",
-          //   dormID: "1",
-          //   floorID: "2",
-          //   roomID: "34",
-          //   lineUID: source.userId
-          // });
-          // const createdStudent = await newStudent.save();
-          // console.log(createdStudent);
         }
+        //#endregion -----初始設定-----
+
+        //#region -----輸入初始資料-----
+        //以正規式檢查資料傳入的是正確格式
+        let rule = /姓名\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+學號\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+第幾宿舍\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+樓層\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+幾房\:/;
+        if(rule.test(message.text)){
+
+          //搜尋此帳號是否已經連結
+          const existStudent = await Student.findOne({lineUID: source.userId});
+          //此帳號已存在
+          if(existStudent){
+
+            //傳送錯誤資訊
+            await client.pushText(source.userId, '此Line帳號已與下方資訊連結 \n如果對此沒印象或想更改 \n請洽管理員進行設定 \n----- \n' + JSON.stringify({
+              'name':existStudent.name,
+              'userID':existStudent.userID,
+            }));
+            
+            //結束
+            res.sendStatus(200);
+            return;
+          }
+
+          //用正規式將輸入拆開，例如"姓名:123\n學號:\n第幾宿舍:7" -> ["姓名", "123", "學號", "", "第幾宿舍", "7"]
+          let input = message.text.split(/[\s,:]/);
+
+          //記錄錯誤
+          let errorMsg = '';
+          //偵測是否有漏輸入的項目
+          if(input[1] == '') errorMsg += '姓名  <-不可為空白\n';
+          if(input[3] == '') errorMsg += '學號  <-不可為空白\n';
+          if(input[5] == '') errorMsg += '宿舍  <-不可為空白\n';
+          if(input[7] == '') errorMsg += '樓層  <-不可為空白\n';
+          if(input[9] == '') errorMsg += '房號  <-不可為空白\n';
+
+          //如果有錯誤訊息
+          if(errorMsg != ''){
+
+            //回傳錯誤訊息
+            errorMsg += '-----\n 請重新輸入';
+            await client.pushText(source.userId, errorMsg);
+            
+            //結束
+            res.sendStatus(200);
+            return;
+          }
+
+          //建立學生model物件
+          const newStudent = new Student({
+            lineUID: source.userId,
+            name: input[1],
+            userID: input[3],
+            dormID: input[5],
+            floorID: input[7],
+            roomID: input[9]
+          });
+
+          //儲存學生資訊
+          const createdStudent = await newStudent.save();
+          
+          //傳送成功資訊
+          await client.pushText(source.userId, '建立成功');
+
+          //結束
+          res.sendStatus(200);
+          return;
+        }
+        //#endregion -----輸入初始資料-----
+
       }
+      
 
       //-----回傳指令類型-----
-      else if(type == "postback"){
-        let data = querystring.parse(event.postback.data);
-        if (data.action === 'url' && data.item === 'clarence') {
-          return client.replyMessage(event.replyToken, {
-            type: 'text',
-            text: 'https://ithelp.ithome.com.tw/users/20117701/ironman/2634'
-          });
-        }
-      }
+      // else if(type == "postback"){
+      //   // let data = querystring.parse(event.postback.data);
+      //   // if (data.action === 'url' && data.item === 'clarence') {
+      //   //   return client.replyMessage(event.replyToken, {
+      //   //     type: 'text',
+      //   //     text: 'https://ithelp.ithome.com.tw/users/20117701/ironman/2634'
+      //   //   });
+      //   // }
+      // }
 
       //-----預料外的內容，開發監測-----
       else{
