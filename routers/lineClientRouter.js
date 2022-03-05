67728e97c5d404d27c3cc9e4722d997b49bd33ed
qmsiteandy <ht93084@gmail.com>
2022-03-05 11:04:29 +0800

    Add: flex訊息-"設定"

--- routers/lineClientRouter.js
+++ routers/lineClientRouter.js
@@ -22,19 +22,22 @@ lineClientRouter.post(
       //-----訊息分類-----
       if(type == "message"){
 
-        //#region -----初始設定-----
-        if(message.text == "初始設定"){
+//#region -----連結設定-----
+        if(message.text == "連結設定"){
         
           //取得template
-          let flexMessage = (require('../template/join-input-template.json'));
+          let flexMessage = (require('../template/connect-input-template.json'));
           //傳送資料到特定id使用者
-          await client.pushFlex(source.userId, 'join-input-template', flexMessage);
+          await client.pushFlex(source.userId, '[connect-input-template]', flexMessage);
 
-          client.pushText(source.userId, '姓名:\n學號:\n第幾宿舍:\n樓層:\n幾房:')
+          //Wait for seconds
+          await new Promise(resolve => setTimeout(resolve, 1000));
+
+          await client.pushText(source.userId, '姓名:\n學號:\n第幾宿舍:\n樓層:\n幾房:')
         }
-        //#endregion -----初始設定-----
+//#endregion -----連結設定-----
 
-        //#region -----輸入初始資料-----
+//#region -----輸入初始資料-----
         //以正規式檢查資料傳入的是正確格式
         let rule = /姓名\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+學號\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+第幾宿舍\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+樓層\:[\u4e00-\u9fa5_a-zA-Z0-9\s]+幾房\:/;
         if(rule.test(message.text)){
@@ -45,7 +48,7 @@ lineClientRouter.post(
           if(existStudent){
 
             //傳送錯誤資訊
-            await client.pushText(source.userId, '此Line帳號已與下方資訊連結 \n如果對此沒印象或想更改 \n請洽管理員進行設定 \n----- \n' + JSON.stringify({
+            await client.pushText(source.userId, '此Line帳號已與下方資訊連結 \n如果對此沒印象或想更改 \n請洽輸入"設定" \n----- \n' + JSON.stringify({
               'name':existStudent.name,
               'userID':existStudent.userID,
             }));
@@ -99,8 +102,44 @@ lineClientRouter.post(
           res.sendStatus(200);
           return;
         }
-        //#endregion -----輸入初始資料-----
+//#endregion -----輸入初始資料-----
 
+//#region -----設定-----
+        if(message.text == "設定"){
+
+          //取得template
+          let flexMessage = (require('../template/setting-tmeplate.json'));
+
+          //搜尋此帳號是否已經連結
+          const existStudent = await Student.findOne({lineUID: source.userId});
+          //已經連結
+          if(existStudent){
+
+            flexMessage['body']['contents'][0]['text'] = "此帳號已經連結下方資訊\n\n" + JSON.stringify({
+              '姓名':existStudent.name,
+              '學號':existStudent.userID,
+              '宿舍':existStudent.dormID,
+              '樓層':existStudent.floorID,
+              '房號':existStudent.roomID
+            }) + '\n\n如需修改資訊\n斷開連結後\n輸入"設定"再次連結';
+
+            console.log(flexMessage['body']['contents'][0]['text']);
+            //刪除用不到的按鈕
+            flexMessage['footer']['contents'].splice(0, 1);
+          }
+          //尚未連接
+          else{
+
+            flexMessage['body']['contents'][0]['text'] = "此帳號尚未連接"
+
+            //刪除用不到的按鈕
+            flexMessage['footer']['contents'].splice(1, 1);
+          }
+
+          //傳送資料到特定id使用者
+          await client.pushFlex(source.userId, '[setting-tmeplate]', flexMessage);
+        }
+//#endregion -----設定-----
       }
       
 
